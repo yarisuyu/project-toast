38af8680915fcad4166aedf869e0287660a1aae6
Elena Oblomova <lisinaeg@gmail.com>
2025-01-22 10:56:59 +0300

    Exercise 2: Show/hide toast

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -10,7 +10,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [selectedOption, setSelectedOption] = React.useState(VARIANT_OPTIONS[0]);
-  const [showToast, setShowToast] = React.useState(true);
+  const [showToast, setShowToast] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -79,7 +79,7 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setShowToast(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
